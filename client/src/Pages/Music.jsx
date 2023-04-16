d7ee5c7ab232d22733a1bb01af2811716e53c28f
iamhk12 <hkothari247@gmail.com>
2023-04-16 14:16:45 +0530

    hk

--- client/src/Pages/Music.jsx
+++ client/src/Pages/Music.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from "react";
+import musicList from './musicList'
 import {
     faPlay,
     faPause,
@@ -6,7 +7,8 @@ import {
     faStepForward,
     // faVolumeMute,
     // faVolumeUp,
-    faExpand
+    faExpand,
+    faSpinner
 } from '@fortawesome/free-solid-svg-icons';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -16,58 +18,6 @@ import './Music.css'
 
 //MUSIC
 
-import music1 from "../components/music/music1.mp3";
-import music2 from "../components/music/music2.mp3";
-import music3 from "../components/music/music3.mp3";
-import music4 from "../components/music/music4.mp3";
-import music5 from "../components/music/music5.mp3";
-import music6 from "../components/music/music6.mp3";
-import music7 from "../components/music/music7.mp3";
-import music8 from "../components/music/music8.mp3";
-import music9 from "../components/music/music9.mp3";
-import music10  from "../components/music/music10.mp3";
-import music11  from "../components/music/music11.mp3";
-import music12  from "../components/music/music12.mp3";
-import music13  from "../components/music/music13.mp3";
-import music14  from "../components/music/music14.mp3";
-import music15  from "../components/music/music15.mp3";
-import music16  from "../components/music/music16.mp3";
-import music17  from "../components/music/music17.mp3";
-import music18  from "../components/music/music18.mp3";
-import music19  from "../components/music/music19.mp3";
-import music20  from "../components/music/music20.mp3";
-import music21  from "../components/music/music21.mp3";
-import music22  from "../components/music/music22.mp3";
-import music23  from "../components/music/music23.mp3";
-import music24  from "../components/music/music24.mp3";
-import music25  from "../components/music/music25.mp3";
-import music26  from "../components/music/music26.mp3";
-import music27  from "../components/music/music27.mp3";
-import music28  from "../components/music/music28.mp3";
-import music29  from "../components/music/music29.mp3";
-import music30  from "../components/music/music30.mp3";
-import music31  from "../components/music/music31.mp3";
-import music32  from "../components/music/music32.mp3";
-import music33  from "../components/music/music33.mp3";
-import music34  from "../components/music/music34.mp3";
-import music35  from "../components/music/music35.mp3";
-import music36  from "../components/music/music36.mp3";
-import music37  from "../components/music/music37.mp3";
-import music38  from "../components/music/music38.mp3";
-import music39  from "../components/music/music39.mp3";
-import music40  from "../components/music/music40.mp3";
-import music41  from "../components/music/music41.mp3";
-import music42  from "../components/music/music42.mp3";
-import music43  from "../components/music/music43.mp3";
-import music44  from "../components/music/music44.mp3";
-import music45  from "../components/music/music45.mp3";
-import music46  from "../components/music/music46.mp3";
-import music47  from "../components/music/music47.mp3";
-import music48  from "../components/music/music48.mp3";
-import music49  from "../components/music/music49.mp3";
-import music50  from "../components/music/music50.mp3";
-import music51  from "../components/music/music51.mp3";
-import music52 from "../components/music/music52.mp3";
 
 let pic1 = 'https://raw.githubusercontent.com/BlazeRahim/InnerCalm/main/client/src/components/Image-1.jpg'
 let pic2 = 'https://raw.githubusercontent.com/BlazeRahim/InnerCalm/main/client/src/components/Image-2.jpg'
@@ -77,11 +27,6 @@ let pic5 = 'https://raw.githubusercontent.com/BlazeRahim/InnerCalm/main/client/s
 let pic6 = 'https://raw.githubusercontent.com/BlazeRahim/InnerCalm/main/client/src/components/Image-6.gif'
 let pic7 = 'https://raw.githubusercontent.com/BlazeRahim/InnerCalm/main/client/src/components/Image-7.gif'
 let pic8 = 'https://raw.githubusercontent.com/BlazeRahim/InnerCalm/main/client/src/components/Image-8.gif'
-const musicList = [music1, music2, music3, music4, music5, music6, music7, music8, music9, music10, music11, 
-    music12, music13, music14, music15, music16, music17, music18, music19, music20, music21, music22, music23, 
-    music24, music25, music26, music27, music28, music29, music30, music31, music32, music33, music34, music35, 
-    music36, music37, music38, music39, music40, music41, music42, music43, music44, music45, music46, music47, 
-    music48, music49, music50, music51, music52];
 
 
 /////////////////
@@ -192,13 +137,33 @@ const ImageSlider = ({ slides }) => {
             elem.requestFullscreen();
         }
     };
+    const [isLoading, setIsLoading] = useState(false);
 
+
+    const handleAudioLoadStart = () => {
+        setIsLoading(true);
+    };
+
+    const handleAudioLoadedData = () => {
+        setIsLoading(false);
+    };
+    const handleAudioEnded = () => {
+        // Call play() method on audio element to replay the music
+        audioRef.current.play();
+        console.log("ended")
+    };
     return (
-        <div style={{ background: "#101316" , height : "100%", width : "100%" }} className="musspage">
+        <div style={{ background: "#101316", height: "100%", width: "100%" }} className="musspage">
             <div style={slideStylesWidthBackground} >
                 <div>
-                    <audio ref={audioRef} src={musicList[currentMusicIndex]}></audio>
-                    <div onClick={goToPrevious} style={leftArrowStyles} className="pageturnbtn"> 
+                    <audio
+                        ref={audioRef}
+                        src={musicList[currentMusicIndex]}
+                        onLoadedData={handleAudioLoadedData}
+                        onLoadStart={handleAudioLoadStart}
+                        onEnded={handleAudioEnded}
+                    ></audio>
+                    <div onClick={goToPrevious} style={leftArrowStyles} className="pageturnbtn">
                         ❰
                     </div>
                     <div onClick={goToNext} style={rightArrowStyles} className="pageturnbtn">
@@ -232,7 +197,7 @@ const ImageSlider = ({ slides }) => {
                                 }
                             }}
                             className="play-button" style={{ cursor: "pointer", fontSize: "30px", border: "none", backgroundColor: "#00000000", color: "#fff", textShadow: "#000 2px 2px 20px" }} >
-                            <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
+                            {isLoading ? <FontAwesomeIcon icon={faSpinner} /> : isPlaying ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} />}
                         </button>
                         <button
                             className="next_prev"
---
+++ client/src/Pages/musicList.js
@@ -0,0 +1,61 @@
+
+import music1 from "../components/music/music1.mp3";
+import music2 from "../components/music/music2.mp3";
+import music3 from "../components/music/music3.mp3";
+import music4 from "../components/music/music4.mp3";
+import music5 from "../components/music/music5.mp3";
+import music6 from "../components/music/music6.mp3";
+import music7 from "../components/music/music7.mp3";
+import music8 from "../components/music/music8.mp3";
+import music9 from "../components/music/music9.mp3";
+import music10 from "../components/music/music10.mp3";
+import music11 from "../components/music/music11.mp3";
+import music12 from "../components/music/music12.mp3";
+import music13 from "../components/music/music13.mp3";
+import music14 from "../components/music/music14.mp3";
+import music15 from "../components/music/music15.mp3";
+import music16 from "../components/music/music16.mp3";
+import music17 from "../components/music/music17.mp3";
+import music18 from "../components/music/music18.mp3";
+import music19 from "../components/music/music19.mp3";
+import music20 from "../components/music/music20.mp3";
+import music21 from "../components/music/music21.mp3";
+import music22 from "../components/music/music22.mp3";
+import music23 from "../components/music/music23.mp3";
+import music24 from "../components/music/music24.mp3";
+import music25 from "../components/music/music25.mp3";
+import music26 from "../components/music/music26.mp3";
+import music27 from "../components/music/music27.mp3";
+import music28 from "../components/music/music28.mp3";
+import music29 from "../components/music/music29.mp3";
+import music30 from "../components/music/music30.mp3";
+import music31 from "../components/music/music31.mp3";
+import music32 from "../components/music/music32.mp3";
+import music33 from "../components/music/music33.mp3";
+import music34 from "../components/music/music34.mp3";
+import music35 from "../components/music/music35.mp3";
+import music36 from "../components/music/music36.mp3";
+import music37 from "../components/music/music37.mp3";
+import music38 from "../components/music/music38.mp3";
+import music39 from "../components/music/music39.mp3";
+import music40 from "../components/music/music40.mp3";
+import music41 from "../components/music/music41.mp3";
+import music42 from "../components/music/music42.mp3";
+import music43 from "../components/music/music43.mp3";
+import music44 from "../components/music/music44.mp3";
+import music45 from "../components/music/music45.mp3";
+import music46 from "../components/music/music46.mp3";
+import music47 from "../components/music/music47.mp3";
+import music48 from "../components/music/music48.mp3";
+import music49 from "../components/music/music49.mp3";
+import music50 from "../components/music/music50.mp3";
+import music51 from "../components/music/music51.mp3";
+import music52 from "../components/music/music52.mp3";
+
+const musicList = [music1, music2, music3, music4, music5, music6, music7, music8, music9, music10, music11,
+    music12, music13, music14, music15, music16, music17, music18, music19, music20, music21, music22, music23,
+    music24, music25, music26, music27, music28, music29, music30, music31, music32, music33, music34, music35,
+    music36, music37, music38, music39, music40, music41, music42, music43, music44, music45, music46, music47,
+    music48, music49, music50, music51, music52];
+
+export default musicList
